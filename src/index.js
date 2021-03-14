687108e45c55736dddaf282639375de8c2776679
Maria <sheyko.mari@mail.ru>
2021-03-14 17:47:01 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix || !matrix.length) {
+    return [];
+  } else {
+    return (matrix[0] + ',' + ('' + matrix[1]).split(",").reverse() + ',' + matrix[2]).split(",");
+  }
 }
