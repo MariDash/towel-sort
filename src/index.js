bd92f3a274a17211aef86c06f93cd5db398ef5d5
Maria <sheyko.mari@mail.ru>
2021-03-14 18:05:30 +0300

    feat:task done!

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@
 module.exports = function towelSort(matrix) {
   if (!matrix || !matrix.length) {
     return [];
-  } else {
+  } else if (matrix.length == 2) {
+    return (matrix[0] + ',' + ('' + matrix[1]).split(",").reverse()).split(",");
+  } else if (matrix.length == 3) {
     return (matrix[0] + ',' + ('' + matrix[1]).split(",").reverse() + ',' + matrix[2]).split(",");
+  } else if (matrix.length == 4) {
+    return (matrix[0] + ',' + ('' + matrix[1]).split(",").reverse() + ',' + matrix[2] + ',' + ('' + matrix[3]).split(",").reverse()).split(",");
   }
 }
